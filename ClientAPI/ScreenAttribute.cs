48cb660b7ca608929ec29ae3ed3af6ab9a06fb01
Vlad <111@11.ru>
2016-03-17 16:42:12 +0300

    Изменил клиентское API

--- ClientAPI/ScreenAttribute.cs
+++ ClientAPI/ScreenAttribute.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClientAPI
 {
-    public interface ILoginScreen
+    [System.AttributeUsage(System.AttributeTargets.Class)]
+    public class ScreenAttribute : System.Attribute
     {
-        event Action<IPAddress, int, string> LogIn;
-
-        void Fail(string message);
     }
 }

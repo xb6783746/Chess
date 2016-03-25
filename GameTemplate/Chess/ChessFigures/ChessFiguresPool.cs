feaec4778f20d7ba158b1d22b804c6724e91b76d
Vlad <111@11.ru>
2016-03-25 16:37:47 +0300

    //

--- GameTemplate/Chess/ChessFigures/ChessFiguresPool.cs
+++ GameTemplate/Chess/ChessFigures/ChessFiguresPool.cs
@@ -18,6 +18,8 @@ namespace GameTemplate.ChessGame.ChessFigures
         public ChessFiguresPool()
         {
             pool = new Dictionary<ChessFType, List<IChessFigure>>();
+
+            Init();
         }
 
 

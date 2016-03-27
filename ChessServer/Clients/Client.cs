3317b93f56d184b719d777159b72d7be77915e36
Vlad <111@11.ru>
2016-03-27 18:19:52 +0300

    Добавил класс клиента

---
+++ ChessServer/Clients/Client.cs
@@ -0,0 +1,36 @@
+﻿using ChessServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessServer.Clients
+{
+    class Client :IClient
+    {
+        public Client(int id, IClientFacade facade)
+        {
+            this.Id = id;
+            this.clientFacade = facade;
+        }
+
+        private IClientFacade clientFacade;
+
+        public int Id
+        {
+            get;
+            private set;
+        }
+        public string Nick
+        {
+            get;
+            set;
+        }
+
+        public void Send(string mesg)
+        {
+            clientFacade.SendMessage(mesg, this.Id);
+        }
+    }
+}
